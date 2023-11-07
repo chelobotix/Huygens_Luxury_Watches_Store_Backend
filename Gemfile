047677a32710b574e2bb02db09b004d6130ddc8b
Marcelo <marceloalarconbarrenechea@gmail.com>
2023-11-07 11:48:03 -0400

    Add Fly.io

--- Gemfile
+++ Gemfile
@@ -64,3 +64,9 @@ group :development do
   # gem "spring"
 end
 
+
+gem "dockerfile-rails", ">= 1.5", :group => :development
+
+gem "sentry-ruby", "~> 5.12"
+
+gem "sentry-rails", "~> 5.12"
--- app/controllers/api/v1/watches_controller.rb
+++ app/controllers/api/v1/watches_controller.rb
@@ -1,96 +1,98 @@
 module Api
   module V1
-    before_action :set_watch, only: %i[show update destroy]
-    before_action :set_watches, only: %i[index]
+    class WatchesController < ApplicationController
+      before_action :set_watch, only: %i[show update destroy]
+      before_action :set_watches, only: %i[index]
 
-    # GET /api/v1/watches
-    # GET /api/v1/watches.json
-    def index
-      details = @watches.map { |watch| details_watch(watch) }
+      # GET /api/v1/watches
+      # GET /api/v1/watches.json
+      def index
+        details = @watches.map { |watch| details_watch(watch) }
 
-      render(json: { status: 'success', watches: details })
-    end
-
-    # GET /api/v1/watches/1
-    # GET /api/v1/watches/1.json
-    def show; end
-
-    # POST /api/v1/watches
-    # POST /api/v1/watches.json
-    def create
-      @watch = Watch.new(watch_params)
-
-      if @watch.save
-        render(:show, status: :created, location: @watch)
-      else
-        render(json: @watch.errors, status: :unprocessable_entity)
+        render(json: { status: 'success', watches: details })
       end
-    end
 
-    # PATCH/PUT /api/v1/watches/1
-    # PATCH/PUT /api/v1/watches/1.json
-    def update
-      if @watch.update(watch_params)
-        render(:show, status: :ok, location: @watch)
-      else
-        render(json: @watch.errors, status: :unprocessable_entity)
+      # GET /api/v1/watches/1
+      # GET /api/v1/watches/1.json
+      def show; end
+
+      # POST /api/v1/watches
+      # POST /api/v1/watches.json
+      def create
+        @watch = Watch.new(watch_params)
+
+        if @watch.save
+          render(:show, status: :created, location: @watch)
+        else
+          render(json: @watch.errors, status: :unprocessable_entity)
+        end
       end
-    end
 
-    # DELETE /api/v1/watches/1
-    # DELETE /api/v1/watches/1.json
-    def destroy
-      @watch.destroy
-    end
+      # PATCH/PUT /api/v1/watches/1
+      # PATCH/PUT /api/v1/watches/1.json
+      def update
+        if @watch.update(watch_params)
+          render(:show, status: :ok, location: @watch)
+        else
+          render(json: @watch.errors, status: :unprocessable_entity)
+        end
+      end
 
-    # Return Genre
-    def genre
-      Rails.logger.debug(watches.genre_id)
-    end
+      # DELETE /api/v1/watches/1
+      # DELETE /api/v1/watches/1.json
+      def destroy
+        @watch.destroy
+      end
 
-    private
+      # Return Genre
+      def genre
+        Rails.logger.debug(watches.genre_id)
+      end
 
-    # Use callbacks to share common setup or constraints between actions.
-    def set_watch
-      @watch = Watch.find(params[:id])
-    end
+      private
 
-    def set_watches
-      @watches = Watch.all
-    end
+      # Use callbacks to share common setup or constraints between actions.
+      def set_watch
+        @watch = Watch.find(params[:id])
+      end
 
-    # Only allow a list of trusted parameters through.
-    def watch_params
-      params.require(:watch).permit(:name, :brand, :price, :video)
-    end
+      def set_watches
+        @watches = Watch.all
+      end
 
-    # Json to return as details to watch
-    def details_watch(watch)
-      {
-        id: watch.id,
-        name: watch.name,
-        price: watch.price,
-        gender: watch.gender.sex,
-        brand: watch.brand.name,
-        video: watch.video,
-        reference: watch.reference,
-        collection: watch.collection,
-        caseSize: watch.caseSize,
-        caseMaterial: watch.caseMaterial,
-        movement: watch.movement,
-        caliber: watch.caliber,
-        powerReserve: watch.powerReserve,
-        dateAdded: watch.dateAdded,
-        dialColor: watch.dialColor,
-        dialMaterial: watch.dialMaterial,
-        caseBack: watch.caseBack,
-        strapMaterial: watch.strapMaterial,
-        strapColor: watch.strapColor,
-        buckleType: watch.buckleType,
-        specialFeature: watch.specialFeatures,
-        thumbnails: watch.thumbnails,
-        images: watch.images
-      }
+      # Only allow a list of trusted parameters through.
+      def watch_params
+        params.require(:watch).permit(:name, :brand, :price, :video)
+      end
+
+      # Json to return as details to watch
+      def details_watch(watch)
+        {
+          id: watch.id,
+          name: watch.name,
+          price: watch.price,
+          gender: watch.gender.sex,
+          brand: watch.brand.name,
+          video: watch.video,
+          reference: watch.reference,
+          collection: watch.collection,
+          caseSize: watch.caseSize,
+          caseMaterial: watch.caseMaterial,
+          movement: watch.movement,
+          caliber: watch.caliber,
+          powerReserve: watch.powerReserve,
+          dateAdded: watch.dateAdded,
+          dialColor: watch.dialColor,
+          dialMaterial: watch.dialMaterial,
+          caseBack: watch.caseBack,
+          strapMaterial: watch.strapMaterial,
+          strapColor: watch.strapColor,
+          buckleType: watch.buckleType,
+          specialFeature: watch.specialFeatures,
+          thumbnails: watch.thumbnails,
+          images: watch.images
+        }
+      end
     end
   end
 end
