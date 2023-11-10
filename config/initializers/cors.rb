ef7ac63c05ec358305b7ae0769ceac665dfac43f
Marcelo <marceloalarconbarrenechea@gmail.com>
2023-11-10 09:47:00 -0400

    Add cors localhost

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://huygenswatches.netlify.app' # Replace '*' with the specific origin(s) you want to allow
+    origins 'https://huygenswatches.netlify.app, http://localhost:5173' # Replace '*' with the specific origin(s) you want to allow
 
     # The resource(s) to allow CORS for
     resource '*',
